e3f00ed3a6149d850df8e6d44165156133c6c6b3
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-05-15 01:56:20 +0300

    New Packaging

--- main.js
+++ main.js
@@ -1,22 +1,64 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Tray, nativeImage} = require('electron')
 const path = require('path')
 
+let tray, window
+
+app.dock?.hide()
+
 function createWindow () {
+  createTray()
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  window = new BrowserWindow({
+    width: 420,
+    height: 560,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
+  window.on('closed', () => window = null)
+
   // and load the index.html of the app.
-  mainWindow.loadURL("http://localhost:3000")
+  window.loadURL("http://localhost:3000")
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+}
+
+const createTray = () => {
+  const icon = path.join(__dirname, 'assets/favicon.png')
+  const nImage = nativeImage.createFromPath(icon)
+
+  tray = new Tray(nImage)
+  tray.on('click', (event) => toggleWindow())
+  tray.setToolTip("Test Electron")
+}
+
+const toggleWindow = () => {
+  window.isVisible() ? window.hide() : showWindow()
+}
+
+const showWindow = () => {
+  const position = windowPosition()
+  window.setPosition(position.x, position.y)
+
+  window.show()
+}
+
+const windowPosition = () => {
+  const windowBounds = window.getBounds()
+  const trayBounds = tray.getBounds()
+
+  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+  const y = Math.round(trayBounds.y + trayBounds.height)
+
+  return {x, y}
 }
 
 // This method will be called when Electron has finished
